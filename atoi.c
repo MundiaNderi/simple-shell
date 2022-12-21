3fc052ef760a701a8db205cbb738144734befd3c
Hess-y <hessy683@gmail.com>
2022-12-21 02:55:59 -0800

    task update

--- atoi.c
+++ atoi.c
@@ -30,8 +30,9 @@ int is_delim(char c, char *delim)
 * @c: The character to input.
 * Return: 1 if c is alphabetic, 0 otherwise.
 */
+int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z)' || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
